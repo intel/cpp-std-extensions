4e0aea8946c3ae063699ac1ce424dff6a3677e00
Ben Deane <ben@elbeno.com>
2023-11-01 19:24:02 -0600

    :sparkles: Add `to_address`

---
+++ include/stdx/memory.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdx/type_traits.hpp>
+
+#include <memory>
+
+namespace stdx {
+inline namespace v1 {
+template <typename T> constexpr auto to_address(T *p) noexcept -> T * {
+    static_assert(not stdx::is_function_v<T>);
+    return p;
+}
+
+namespace detail::detect {
+template <typename T, typename = void>
+constexpr auto pointer_traits_to_address = false;
+
+template <typename T>
+constexpr auto pointer_traits_to_address<
+    T, std::void_t<decltype(std::pointer_traits<T>::to_address(
+           std::declval<T>()))>> = true;
+} // namespace detail::detect
+
+template <typename T> constexpr auto to_address(T const &t) {
+    if constexpr (detail::detect::pointer_traits_to_address<T>) {
+        return std::pointer_traits<T>::to_address(t);
+    } else {
+        return to_address(t.operator->());
+    }
+}
+} // namespace v1
+} // namespace stdx
