412b4188ee14d83dabbc34df300d0d0b0624d8a7
Ben Deane <ben@elbeno.com>
2024-03-01 08:51:16 -0700

    :sparkles: Add variadic `transform`
    `std::transform` has issues:
    - it only has unary and binary overloads
    - it violates the principle of useful return
    
    `std::ranges::views::zip_transform` solves these issues, but at the considerable
    expense of ranges machinery.

---
+++ include/stdx/algorithm.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <functional>
+#if __cplusplus >= 202002L
+#include <stdx/tuple.hpp>
+#else
+#include <tuple>
+#endif
+
+namespace stdx {
+inline namespace v1 {
+namespace detail {
+#if __cplusplus >= 202002L
+template <typename... Ts> using result_tuple_t = stdx::tuple<Ts...>;
+#define CONSTEXPR_INVOKE constexpr
+#else
+template <typename... Ts> using result_tuple_t = std::tuple<Ts...>;
+#define CONSTEXPR_INVOKE
+#endif
+} // namespace detail
+
+template <typename O, typename... Is>
+using transform_result = detail::result_tuple_t<O, Is...>;
+
+template <typename InputIt, typename OutputIt, typename Operation,
+          typename... InputItN>
+CONSTEXPR_INVOKE auto transform(InputIt first, InputIt last, OutputIt d_first,
+                                Operation op, InputItN... first_n)
+    -> transform_result<OutputIt, InputIt, InputItN...> {
+    while (first != last) {
+        *d_first = std::invoke(op, *first, *first_n...);
+        static_cast<void>(++first), (static_cast<void>(++first_n), ...),
+            ++d_first;
+    }
+    return {d_first, first, first_n...};
+}
+
+template <typename InputIt, typename Size, typename OutputIt,
+          typename Operation, typename... InputItN>
+CONSTEXPR_INVOKE auto transform_n(InputIt first, Size n, OutputIt d_first,
+                                  Operation op, InputItN... first_n)
+    -> transform_result<OutputIt, InputIt, InputItN...> {
+    while (n-- > 0) {
+        *d_first = std::invoke(op, *first, *first_n...);
+        static_cast<void>(++first), (static_cast<void>(++first_n), ...),
+            ++d_first;
+    }
+    return {d_first, first, first_n...};
+}
+
+#undef CONSTEXPR_INVOKE
+} // namespace v1
+} // namespace stdx
