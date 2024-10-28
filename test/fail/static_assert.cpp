9f240cbc65c3e7069eac9a3d2dcd1520a7d9baeb
Ben Deane <ben@elbeno.com>
2024-10-28 08:38:42 -0600

    :art: Add `STATIC_ASSERT`
    Problem:
    - `ct_check<cond>.template emit<msg>()` is not so ergonomic to use.
    
    Solution:
    - Add `STATIC_ASSERT` which does approximately the same thing, but in a more
      easy-to-use form.

---
+++ test/fail/static_assert.cpp
@@ -0,0 +1,11 @@
+#include <stdx/ct_string.hpp>
+
+// EXPECT: 01234567890123456789012345678901234567890123456789
+
+constexpr auto msg =
+    stdx::ct_string{"01234567890123456789012345678901234567890123456789"};
+
+auto main() -> int {
+    static_assert(STATIC_ASSERT(true, "not emitted"));
+    STATIC_ASSERT(false, msg);
+}
