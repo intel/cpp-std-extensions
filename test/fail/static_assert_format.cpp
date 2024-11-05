5ffce28ccae865e7c75b6bb7308bba7671ec198c
Ben Deane <ben@elbeno.com>
2024-11-05 14:07:47 -0700

    :art: Allow bare template arguments in `STATIC_ASSERT`
    Problem:
    - `STATIC_ASSERT("{}", 42)` should be allowed, because `42` can be a template
      argument.
    
    Solution:
    - Allow it.

--- test/fail/static_assert_format.cpp
+++ test/fail/static_assert_format.cpp
@@ -1,14 +1,10 @@
 #include <stdx/static_assert.hpp>
 #include <stdx/utility.hpp>
 
-#include <string_view>
-
 // EXPECT: hello world int 123
 
 template <typename T> constexpr auto f() {
-    using namespace std::string_view_literals;
-    STATIC_ASSERT(false, "hello {} {} {}", CX_VALUE("world"sv), CX_VALUE(T),
-                  CX_VALUE(123));
+    STATIC_ASSERT(false, "hello {} {} {}", CX_VALUE("world"), CX_VALUE(T), 123);
 }
 
 auto main() -> int { f<int>(); }
