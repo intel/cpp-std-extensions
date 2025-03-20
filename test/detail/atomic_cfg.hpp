bed2843eea345ef2d1677a48ef4889d097e2d470
Ben Deane <ben@elbeno.com>
2025-03-20 16:37:06 -0600

    :bug: Make `stdx::atomic` work with scoped enumerations
    Problem:
    - Configuring `stdx::atomic` for scoped enumerations doesn't work because a
      scoped enumeration is not convertible to its configured underlying type.
    
    Solution:
    - Instead of requiring convertibility, require the types to be trivially
      copyable, and that the size & alignment is compatible.

--- test/detail/atomic_cfg.hpp
+++ test/detail/atomic_cfg.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <type_traits>
 
 template <> struct atomic::atomic_type<std::uint8_t> {
     using type = std::uint32_t;
@@ -13,3 +14,10 @@ template <> struct atomic::atomic_type<bool> {
 template <typename T> struct atomic::atomic_type<T *> {
     using type = std::uintptr_t;
 };
+
+#if __cplusplus >= 202002L
+template <typename T>
+    requires(std::is_enum_v<T>)
+struct atomic::atomic_type<T> : atomic::atomic_type<std::underlying_type_t<T>> {
+};
+#endif
