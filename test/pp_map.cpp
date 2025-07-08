6c51fa8c4a9132bf20ad7477117c70d1d74ec4f1
Ben Deane <ben@elbeno.com>
2025-07-08 17:01:59 -0600

    :sparkles: Add preprocessor mapping function
    Problem:
    - It's useful to have `map`/`transform` in the preprocessor.
    
    Solution:
    - Add `pp_map.hpp`.

---
+++ test/pp_map.cpp
@@ -0,0 +1,31 @@
+#include <stdx/compiler.hpp>
+#include <stdx/pp_map.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace {
+auto count_args = [](auto const &...args) { return sizeof...(args); };
+} // namespace
+
+TEST_CASE("pp_map zero arguments", "[pp_map]") {
+#ifdef __clang__
+    STDX_PRAGMA(diagnostic push)
+    STDX_PRAGMA(diagnostic ignored "-Wgnu-zero-variadic-macro-arguments")
+#endif
+    static_assert(count_args(STDX_MAP(int)) == 0);
+#ifdef __clang__
+    STDX_PRAGMA(diagnostic pop)
+#endif
+}
+
+TEST_CASE("pp_map one argument", "[pp_map]") {
+    static_assert(count_args(STDX_MAP(double, 1)) == 1.0);
+}
+
+TEST_CASE("pp_map n arguments", "[pp_map]") {
+    static_assert(count_args(STDX_MAP(double, 1, 2, 3)) == 3);
+}
+
+TEST_CASE("pp_map parenthesized arguments", "[pp_map]") {
+    static_assert(count_args(STDX_MAP(double, ((void)1, 2), 3)) == 2);
+}
