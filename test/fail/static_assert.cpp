5bec6e6a7d77b136a7542a146b8e5b6da624e579
Ben Deane <ben@elbeno.com>
2025-11-03 20:14:55 -0700

    :bug: Fix `STATIC_ASSERT` in non-local context
    Problem:
    - The lambda used in `STATIC_ASSERT` has a default capture spec, which is not
      allowed in non-local contexts.
    
    Solution:
    - Remove the capture spec. It's not needed.

--- test/fail/static_assert.cpp
+++ test/fail/static_assert.cpp
@@ -5,6 +5,8 @@
 constexpr auto msg =
     stdx::ct_string{"01234567890123456789012345678901234567890123456789"};
 
+static_assert(STATIC_ASSERT(true, "not emitted"));
+
 auto main() -> int {
     static_assert(STATIC_ASSERT(true, "not emitted"));
     STATIC_ASSERT(false, msg);
