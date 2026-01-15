4c4dd60232926140cba3f4d38b715915ebb0a570
Ben Deane <ben@elbeno.com>
2026-01-15 11:10:06 -0700

    :arrow_up: Bump actions/setup-node from 6.1.0 to 6.2.0

--- test/pp_map.cpp
+++ test/pp_map.cpp
@@ -11,6 +11,7 @@ TEST_CASE("pp_map zero arguments", "[pp_map]") {
 #ifdef __clang__
     STDX_PRAGMA(diagnostic push)
     STDX_PRAGMA(diagnostic ignored "-Wgnu-zero-variadic-macro-arguments")
+    STDX_PRAGMA(diagnostic ignored "-Wc++20-extensions")
 #endif
     static_assert(count_args(STDX_MAP(int)) == 0);
 #ifdef __clang__
